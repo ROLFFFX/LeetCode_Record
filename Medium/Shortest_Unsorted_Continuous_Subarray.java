ed55a1b2b18bbf77264d55603f3381e104d8a233
Yuxuan Shi <1051268839@qq.com>
2023-11-08 02:25:09 -0500

    Update 1108

--- Medium/Shortest_Unsorted_Continuous_Subarray.java
+++ Medium/Shortest_Unsorted_Continuous_Subarray.java
@@ -5,14 +5,29 @@ public class Shortest_Unsorted_Continuous_Subarray {
         int[] nums = {2,6,4,8,10,9,15}; //5
         int[] nums2 = {1,2,3,4};
         int[] nums3 = {2,1};
-        int ans = findUnsortedSubarray(nums2);
+        int ans = findUnsortedSubarray(nums3);
         System.out.println(ans);
     }
     public static int findUnsortedSubarray(int[] nums) {
-        int min = 0;
-        int max = 10^5;
-        
-
-        return 1;
+        int len = nums.length - 1;
+        int left = -1;
+        int right = -1;
+        int max = nums[0];
+        int min = nums[len];
+        for (int i = 1; i <= len; i++){
+            int a = nums[i];
+            int b = nums[len - i];
+            if (a < max){
+                right = i;
+            } else {
+                max = a;
+            }
+            if (b > min){
+                left = i;
+            } else {
+                min = b;
+            }
+        }
+        return Math.max(0, left + right - len + 1);
     }
 }
