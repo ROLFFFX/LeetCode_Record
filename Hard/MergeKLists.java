7d657e2826739f83aff6ebdac81b81d3794bb732
Yuxuan Shi <1051268839@qq.com>
2023-12-22 23:53:42 +0900

    Update MergeKLists

---
+++ Hard/MergeKLists.java
@@ -0,0 +1,13 @@
+package Hard;
+
+public class MergeKLists {
+//    You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+//    Merge all the linked-lists into one sorted linked-list and return it.
+    public static void main(String[] args) {
+        int[][] lists = {{1,4,5},{1,3,4},{2,6}};
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+
+
+    }
+}
