6d5af63f2eb399e2620589e0c140c8585995a216
Yuxuan Shi <1051268839@qq.com>
2023-12-23 22:34:59 +0900

    Update Remove_Linked_List_Elements.java

--- Easy/Remove_Linked_List_Elements/Remove_Linked_List_Elements.java
+++ Easy/Remove_Linked_List_Elements/Remove_Linked_List_Elements.java
@@ -9,35 +9,33 @@ public class Remove_Linked_List_Elements {
         removeElements(test, 1);
 
     }
+
     public static ListNode removeElements(ListNode head, int val) {
         if (head == null){ return null; }
-        ListNode dummy = head;
-        while (true){
+        if (head.next == null){
+            ListNode ret = (head.val == val) ? null : head;
+            return ret;
+        }
+        // else traverse list
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        head = dummy;
+        ListNode ans = head;
+        while (head.next.next != null){
+            if (head.next.val == val){
+                head.next = head.next.next;
+                continue;
+            }
+            if (head.next != null){
+                head = head.next;
+            } else {
+                break;
+            }
 
         }
+        if (head.next.val == val){
+            head.next = null;
+        }
+        return ans.next;
     }
-
-
-
-//    public static ListNode removeElements(ListNode head, int val) {
-//        if (head == null){ return null; }
-//        if (head.next == null){
-//            ListNode ret = (head.val == val) ? null : head;
-//            return ret;
-//        }
-//        // else traverse list
-//        ListNode dummy = head;
-//        while (head.next != null){
-//            if (head.next.val == val){
-//                head.next = head.next.next;
-//            }
-//            if (head.next != null){
-//                head = head.next;
-//            } else {
-//                break;
-//            }
-//
-//        }
-//        return dummy;
-//    }
 }
