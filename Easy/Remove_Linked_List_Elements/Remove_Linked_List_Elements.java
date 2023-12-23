920cd3268ec826ad2494159d8a9793060db09da1
Yuxuan Shi <1051268839@qq.com>
2023-12-23 22:40:25 +0900

    Update Remove_Linked_List_Elements.java

--- Easy/Remove_Linked_List_Elements/Remove_Linked_List_Elements.java
+++ Easy/Remove_Linked_List_Elements/Remove_Linked_List_Elements.java
@@ -7,35 +7,51 @@ public class Remove_Linked_List_Elements {
         boolean flag = test.next == null;
         System.out.println();
         removeElements(test, 1);
-
     }
 
     public static ListNode removeElements(ListNode head, int val) {
         if (head == null){ return null; }
-        if (head.next == null){
-            ListNode ret = (head.val == val) ? null : head;
-            return ret;
-        }
-        // else traverse list
         ListNode dummy = new ListNode();
         dummy.next = head;
         head = dummy;
         ListNode ans = head;
-        while (head.next.next != null){
+        while (head.next != null){
             if (head.next.val == val){
                 head.next = head.next.next;
-                continue;
-            }
-            if (head.next != null){
-                head = head.next;
             } else {
-                break;
+                head = head.next;
             }
-
         }
-        if (head.next.val == val){
-            head.next = null;
-        }
-        return ans.next;
+        return ans;
     }
+
+/** Success but noe optimized**/
+//    public static ListNode removeElements(ListNode head, int val) {
+//        if (head == null){ return null; }
+//        if (head.next == null){
+//            ListNode ret = (head.val == val) ? null : head;
+//            return ret;
+//        }
+//        // else traverse list
+//        ListNode dummy = new ListNode();
+//        dummy.next = head;
+//        head = dummy;
+//        ListNode ans = head;
+//        while (head.next.next != null){
+//            if (head.next.val == val){
+//                head.next = head.next.next;
+//                continue;
+//            }
+//            if (head.next != null){
+//                head = head.next;
+//            } else {
+//                break;
+//            }
+//
+//        }
+//        if (head.next.val == val){
+//            head.next = null;
+//        }
+//        return ans.next;
+//    }
 }
