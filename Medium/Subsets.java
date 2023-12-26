67342419e86092089876be60629e39e708d2257b
Yuxuan Shi <1051268839@qq.com>
2023-12-26 15:23:34 -0800

    Create Subsets.java

---
+++ Medium/Subsets.java
@@ -0,0 +1,15 @@
+package Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        return ans;
+    }
+}
