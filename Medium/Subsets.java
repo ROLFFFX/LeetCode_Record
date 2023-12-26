9598a66006b53102e45d2b864fe68b5a4d584f5c
Yuxuan Shi <1051268839@qq.com>
2023-12-26 15:26:53 -0800

    Update Subsets.java

--- Medium/Subsets.java
+++ Medium/Subsets.java
@@ -1,6 +1,7 @@
 package Medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Subsets {
@@ -8,8 +9,18 @@ public class Subsets {
 
     }
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
 
-        return ans;
+    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+        list.add(new ArrayList<>(tempList));
+        for(int i = start; i < nums.length; i++){
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
     }
 }
