78344570e1220facd044571a3af843415d8ddf8c
Yuxuan Shi <1051268839@qq.com>
2024-01-01 11:55:25 -0500

    Update Merge_String_Alternatively.java

--- Easy/Merge_String_Alternatively.java
+++ Easy/Merge_String_Alternatively.java
@@ -1,6 +1,22 @@
 package Easy;
 
 public class Merge_String_Alternatively {
+    public String mergeAlternately_2(String word1, String word2) {
+        String merged = "";
+        int i = 0;
+        int j = 0;
+        while (i < word1.length() || j < word2.length()){
+            if (i < word1.length()){
+                merged+=word1.charAt(i);
+            }
+            if (j < word2.length()){
+                merged+=word2.charAt(j);
+            }
+            i++;
+            j++;
+        }
+        return merged;
+    }
     public String mergeAlternately(String word1, String word2) {
         int index = Math.min(word1.length(), word2.length());
         String merged = "";
