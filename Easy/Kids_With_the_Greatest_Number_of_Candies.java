cf63dffcc2dc87ddd2c39cb715b294732850ec32
Yuxuan Shi <1051268839@qq.com>
2024-01-01 13:03:54 -0500

    Update Kids_With_the_Greatest_Number_of_Candies.java

--- Easy/Kids_With_the_Greatest_Number_of_Candies.java
+++ Easy/Kids_With_the_Greatest_Number_of_Candies.java
@@ -1,6 +1,21 @@
 package Easy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Kids_With_the_Greatest_Number_of_Candies {
+    public List<Boolean> kidsWithCandies_2(int[] candies, int extraCandies) {
+        int max = 0;
+        for (int candy : candies){
+            max = Math.max(max, candy);
+        }
+        //max now have max candies
+        List<Boolean> ans = new ArrayList();
+        for (int candy : candies){
+            ans.add(candy + extraCandies >= max);
+        }
+        return ans;
+    }
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int max = 0;
         for (int candy : candies){
