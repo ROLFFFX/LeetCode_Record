089e38f130191196d7acec23c2537505146b25b8
Yuxuan Shi <1051268839@qq.com>
2024-01-01 20:25:57 -0500

    Create MoveZeroes.java

---
+++ Easy/MoveZeroes.java
@@ -0,0 +1,16 @@
+package Easy;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int sb = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == 0){
+                sb++;
+            } else if (sb > 0) {
+                int temp = nums[i];
+                nums[i] = 0;
+                nums[i - sb] = temp;
+            }
+        }
+    }
+}
