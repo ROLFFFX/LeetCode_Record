84dac6d96a3d6c0f3cf26db12ed812da290333e4
Yuxuan Shi <1051268839@qq.com>
2024-01-02 18:42:41 -0500

    Update Max_Number_Of_K_Sum_Pairs

--- Medium/Max_Number_Of_K_Sum_Pairs.java
+++ Medium/Max_Number_Of_K_Sum_Pairs.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Max_Number_Of_K_Sum_Pairs {
     public static void main(String[] args) {
         Max_Number_Of_K_Sum_Pairs solution = new Max_Number_Of_K_Sum_Pairs();
-        System.out.println(solution.maxOperations(new int[]{3, 1, 3, 4, 3}, 6));
+        System.out.println(solution.maxOperations(new int[]{4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4}, 2));
     }
 
     public static int maxOperations(int[] nums, int k) {
@@ -15,7 +15,6 @@ public class Max_Number_Of_K_Sum_Pairs {
         }
         int count = 0;
         for (int i = 0; i < list.size(); i++){
-            int sum = 0;
             int curr = list.get(i);
             if (curr > k){
                 continue;
@@ -23,9 +22,13 @@ public class Max_Number_Of_K_Sum_Pairs {
             for (int j = i + 1; j < list.size(); j++){
                 if (curr + list.get(j) == k){
                     count++;
+//                    System.out.println("Removing: ");
+//                    System.out.println(list.get(i));
+//                    System.out.println(list.get(j));
                     list.remove(i);
                     list.remove(j - 1);
                     i--;
+                    break;
                 }
             }
         }
