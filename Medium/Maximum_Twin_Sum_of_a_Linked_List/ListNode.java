09b181c1b9a7a324866fc0592fa53ed20788cd63
Yuxuan Shi <1051268839@qq.com>
2024-01-04 18:14:49 -0500

    Created Maximum_Twin_Sum_of_a_Linked_List

---
+++ Medium/Maximum_Twin_Sum_of_a_Linked_List/ListNode.java
@@ -0,0 +1,9 @@
+package Medium.Maximum_Twin_Sum_of_a_Linked_List;
+
+ public class ListNode {
+     int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
---
+++ Medium/Maximum_Twin_Sum_of_a_Linked_List/Maximum_Twin_Sum_of_a_Linked_List.java
@@ -0,0 +1,30 @@
+package Medium.Maximum_Twin_Sum_of_a_Linked_List;
+
+public class Maximum_Twin_Sum_of_a_Linked_List {
+    public int pairSum(ListNode head) {
+        // reverse a linked list and see max pair sum
+        ListNode reversed = reverse(head);
+        ListNode curr = head;
+        int max = 0;
+        while (curr.next != null){
+            max = Math.max(max, curr.val + reversed.val);
+            curr = curr.next;
+            reversed = reversed.next;
+        }
+        return max;
+    }
+
+    // reverse linked list
+    public ListNode reverse(ListNode head){
+        ListNode prev = new ListNode(0);
+        prev.next = null;
+        ListNode curr = head;
+        while (curr != null){
+            ListNode newNode = new ListNode(curr.val);
+            newNode.next = prev.next;
+            prev.next = newNode;
+            curr = curr.next;
+        }
+        return prev.next;
+    }
+}
