bc722a586aaa909492f39c121894eddd15db1699
Yuxuan Shi <1051268839@qq.com>
2024-01-05 19:33:09 -0500

    Create Asteroid_Collision.java

---
+++ Medium/Asteroid_Collision.java
@@ -0,0 +1,30 @@
+package Medium;
+
+import java.util.Stack;
+
+public class Asteroid_Collision {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int ast : asteroids) {
+            collision:
+            {
+                while (!stack.isEmpty() && ast < 0 && 0 < stack.peek()) {
+                    if (stack.peek() < -ast) {
+                        stack.pop();
+                        continue;
+                    } else if (stack.peek() == -ast) {
+                        stack.pop();
+                    }
+                    break collision;
+                }
+                stack.push(ast);
+            }
+        }
+
+        int[] ans = new int[stack.size()];
+        for (int i = ans.length - 1; i >= 0; i--) {
+            ans[i] = stack.pop();
+        }
+        return ans;
+    }
+}
