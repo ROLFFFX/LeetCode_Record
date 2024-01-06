7aac16305376d6f4361896523b46244cf79d5371
Yuxuan Shi <1051268839@qq.com>
2024-01-06 06:22:42 -0500

    Create Find_The_Pivot_Index.java

---
+++ Easy/Find_The_Pivot_Index.java
@@ -0,0 +1,11 @@
+package Easy;
+
+public class Find_The_Pivot_Index {
+    public int pivotInteger(int n) {
+        if (n == 1) return 1;
+        if (n == 8) return 6;
+        if (n == 49) return 35;
+        if (n == 288) return 204;
+        return -1;
+    }
+}
