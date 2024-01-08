97c0dc4139f742958b7a7ab1ef1abe6482492090
Yuxuan Shi <1051268839@qq.com>
2024-01-07 22:24:19 -0500

    Update /out

--- Hard/Count_the_Number_of_Powerful_Integers.java
+++ Hard/Count_the_Number_of_Powerful_Integers.java
@@ -11,7 +11,13 @@ public class Count_the_Number_of_Powerful_Integers {
 //    A string x is a suffix of a string y if and only if x is a substring
 //    of y that starts from some index (including 0) in y and extends to the
 //    index y.length - 1. For example, 25 is a suffix of 5125 whereas 512 is not.
-    public long numberOfPowerfulInt(long start, long finish, int limit, String s) {
+    public static void main(String[] args) {
+        long ans = numberOfPowerfulInt(1,6000,4,"124");
+        System.out.println(ans);
+    }
+    public static long numberOfPowerfulInt(long start, long finish, int limit, String s) {
+
+
         return 1;   //dummy
     }
 }
