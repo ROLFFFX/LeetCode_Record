f47ca2ca922101d1ad24348dfa25746cfadd214c
Yuxuan Shi <1051268839@qq.com>
2024-01-08 20:44:18 -0500

    Create Number_of_Students_Unable_to_Eat_Lunch.java

---
+++ Easy/Number_of_Students_Unable_to_Eat_Lunch.java
@@ -0,0 +1,19 @@
+package Easy;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Number_of_Students_Unable_to_Eat_Lunch {
+    public int countStudents(int[] students, int[] sandwiches) {
+        Stack<Integer> sandwich = new Stack();
+        for (int i = sandwiches.length - 1; i >=0; i--){
+            sandwich.push(sandwiches[i]);
+        }
+        List<Integer> student = new ArrayList();
+        for (int i : students) student.add(i);
+        while (!sandwich.isEmpty() && student.contains(sandwich.peek())){
+            student.remove(sandwich.pop());
+        }
+        return student.size();
+    }
+}
