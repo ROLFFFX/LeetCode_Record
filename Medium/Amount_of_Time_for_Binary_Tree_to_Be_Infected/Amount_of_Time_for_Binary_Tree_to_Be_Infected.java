8ca6fb231c6fb71e6ac97517beb43f691de9160c
Yuxuan Shi <1051268839@qq.com>
2024-01-09 20:11:34 -0500

    Created Amount_of_Time_for_Binary_Tree_to_Be_Infected

---
+++ Medium/Amount_of_Time_for_Binary_Tree_to_Be_Infected/Amount_of_Time_for_Binary_Tree_to_Be_Infected.java
@@ -0,0 +1,38 @@
+package Medium.Amount_of_Time_for_Binary_Tree_to_Be_Infected;
+
+import java.util.*;
+
+public class Amount_of_Time_for_Binary_Tree_to_Be_Infected {
+    public int amountOfTime(TreeNode root, int start) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        createGraph(root, graph);
+        return maxDistance(graph, new HashSet<>(), start, 0, 0);
+    }
+
+    private void createGraph(TreeNode root, Map<Integer, List<Integer>> graph) {
+        List<Integer> adjacent = graph.computeIfAbsent(root.val, parameter -> new ArrayList<>());
+
+        if (root.left != null) {
+            graph.computeIfAbsent(root.left.val, param -> new ArrayList<>()).add(root.val);
+            adjacent.add(root.left.val);
+            createGraph(root.left, graph);
+        }
+        if (root.right != null) {
+            graph.computeIfAbsent(root.right.val, param -> new ArrayList<>()).add(root.val);
+            adjacent.add(root.right.val);
+            createGraph(root.right, graph);
+        }
+    }
+
+    private int maxDistance(Map<Integer, List<Integer>> graph, Set<Integer> visited, int currentNode, int maxDistance, int currentDistance) {
+        if (!visited.contains(currentNode)) {
+            visited.add(currentNode);
+            maxDistance = Math.max(maxDistance, currentDistance);
+
+            for (int neighbour : graph.get(currentNode)) {
+                maxDistance = Math.max(maxDistance(graph, visited, neighbour, maxDistance, currentDistance + 1), maxDistance);
+            }
+        }
+        return maxDistance;
+    }
+}
---
+++ Medium/Amount_of_Time_for_Binary_Tree_to_Be_Infected/TreeNode.java
@@ -0,0 +1,14 @@
+package Medium.Amount_of_Time_for_Binary_Tree_to_Be_Infected;
+
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
