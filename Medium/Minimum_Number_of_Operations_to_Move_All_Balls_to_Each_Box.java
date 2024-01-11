ad17532ccc25c0c01f928c1d3f015c4c790aeafa
Yuxuan Shi <1051268839@qq.com>
2024-01-11 09:48:50 -0500

    Update Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.java

--- Medium/Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.java
+++ Medium/Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.java
@@ -1,4 +1,19 @@
 package Medium;
 
 public class Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box {
+    public int[] minOperations(String boxes) {
+        int[] ans = new int[boxes.length()];
+        char[] box = boxes.toCharArray();
+        for (int i = 0; i < box.length; i++){
+            // for each digit in input
+            int curr_sum = 0;
+            for (int j = 0; j < box.length; j++){
+                if (j == i) continue;
+                if (box[j] == '0') continue;
+                curr_sum += Math.abs(j - i);
+            }
+            ans[i] = curr_sum;
+        }
+        return ans;
+    }
 }
