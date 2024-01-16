7b2431234ce48940fc454c273226bf4bf8c73bbe
Yuxuan Shi <1051268839@qq.com>
2024-01-16 17:59:27 -0500

    Created Separate_the_Digits_in_an_Array

--- Easy/Separate_the_Digits_in_an_Array.java
+++ Easy/Separate_the_Digits_in_an_Array.java
@@ -3,18 +3,18 @@ package Easy;
 import java.util.Arrays;
 
 public class Separate_the_Digits_in_an_Array {
+    static int[] test = {1,2,3,4};
     public static void main(String[] args) {
-        Separate_the_Digits_in_an_Array solution =
-                new Separate_the_Digits_in_an_Array();
-        System.out.println(solution);
+        int[] ans = separateDigits(test);
+        System.out.println(ans);
     }
-    public static  int[] separateDigits(int[] nums) {
-        String s = "";
-        for (int num : nums) s += String.valueOf(num);
+    public static int[] separateDigits(int[] nums) {
+        StringBuilder sb = new StringBuilder();
+        for (int num : nums) sb.append(num);
+        String s = sb.toString();
         int[] ans = new int[s.length()];
-        int i = 0;
-        for (char c : s.toCharArray()){
-            ans[i] = c;
+        for (int i = 0; i < s.length(); i++) {
+            ans[i] = s.charAt(i) - '0';
         }
         return ans;
     }
