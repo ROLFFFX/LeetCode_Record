51e04192dc98aa1f9d75ddd64219592ca1eb0a84
Yuxuan Shi <1051268839@qq.com>
2024-01-17 12:59:58 -0500

    Create Count_Good_Triplets.java

---
+++ Easy/Count_Good_Triplets.java
@@ -0,0 +1,20 @@
+package Easy;
+
+public class Count_Good_Triplets {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int z= 0;
+        for (int i = 0; i < arr.length; i++){
+            for (int j = i + 1; j < arr.length; j++){
+                if (Math.abs(arr[i] - arr[j]) <= a){
+                    for (int k = j + 1; k < arr.length; k++){
+                        if(Math.abs(arr[j] - arr[k]) <= b &&
+                                Math.abs(arr[i] - arr[k]) <= c){
+                            z++;
+                        }
+                    }
+                }
+            }
+        }
+        return z;
+    }
+}
