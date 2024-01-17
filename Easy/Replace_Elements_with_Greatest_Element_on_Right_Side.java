7586a48050d78a5485de3143b11d0219eb56cd84
Yuxuan Shi <1051268839@qq.com>
2024-01-17 08:01:27 -0500

    Update Replace_Elements_with_Greatest_Element_on_Right_Side.java

--- Easy/Replace_Elements_with_Greatest_Element_on_Right_Side.java
+++ Easy/Replace_Elements_with_Greatest_Element_on_Right_Side.java
@@ -1,6 +1,19 @@
 package Easy;
 
 public class Replace_Elements_with_Greatest_Element_on_Right_Side {
+    public int[] replaceElements_optimized(int[] arr) {
+        int max = -1;
+        for (int i = arr.length-1; i >= 0; i--){
+            if (arr[i] > max){
+                int temp = max;
+                max = arr[i];
+                arr[i] = temp;
+            } else {
+                arr[i] = max;
+            }
+        }
+        return arr;
+    }
     public int[] replaceElements(int[] arr) {
         for (int i = 0; i < arr.length; i++){
             arr[i] = find(arr, i);
