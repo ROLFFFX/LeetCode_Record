ab8131402c5bb3aaafcd2b68719ba7c322b38957
Yuxuan Shi <1051268839@qq.com>
2024-01-17 14:13:04 -0500

    Update Univalued_Binary_Tree.java

--- Easy/Trees/Univalued_Binary_Tree.java
+++ Easy/Trees/Univalued_Binary_Tree.java
@@ -4,6 +4,12 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Univalued_Binary_Tree {
+    public boolean isUnivalTree_DFS(TreeNode root) {
+        if (root == null) return true;
+        if (root.left != null && root.left.val != root.val) return false;
+        if (root.right != null && root.right.val != root.val) return false;
+        return isUnivalTree(root.left) && isUnivalTree(root.right);
+    }
     public boolean isUnivalTree(TreeNode root) {
         int val = root.val;
         Queue<TreeNode> q = new LinkedList();
