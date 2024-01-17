0f9b4a715914dae7d597b03e9913356155ece469
Yuxuan Shi <1051268839@qq.com>
2024-01-17 13:22:47 -0500

    Create Minimum_Operations_to_Make_All_Array_Elements_Equal.java

---
+++ Medium/Minimum_Operations_to_Make_All_Array_Elements_Equal.java
@@ -0,0 +1,30 @@
+package Medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Minimum_Operations_to_Make_All_Array_Elements_Equal {
+    public List<Long> minOperations(int[] nums, int[] queries) {
+        Arrays.sort(nums);
+        List<Long> ans = new ArrayList<>();
+        int n = nums.length;
+        long[] prefix = new long[n + 1];
+        for (int i = 1; i <= n; i++)
+            prefix[i] = prefix[i - 1] + nums[i - 1];
+        for (int x: queries) {
+            int i = bisect_left(nums, x);
+            ans.add(1L * x * (2 * i - n) + prefix[n] - 2 * prefix[i]);
+        }
+        return ans;
+    }
+    private int bisect_left(int[] nums, int x) {
+        int lo = 0, hi = nums.length;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] < x) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
+    }
+}
