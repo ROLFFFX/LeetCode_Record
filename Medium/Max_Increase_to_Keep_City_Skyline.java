f8826a96d1c43e778055d68e050d7cda83530696
Yuxuan Shi <1051268839@qq.com>
2024-01-17 12:42:43 -0500

    Update Max_Increase_to_Keep_City_Skyline.java

--- Medium/Max_Increase_to_Keep_City_Skyline.java
+++ Medium/Max_Increase_to_Keep_City_Skyline.java
@@ -1,6 +1,24 @@
 package Medium;
 
 public class Max_Increase_to_Keep_City_Skyline {
+    public int maxIncreaseKeepingSkyline_optimized(int[][] grid) {
+        // store column maxes and row maxes
+        int[] rowMax = new int[grid.length];
+        int[] colMax = new int[grid[0].length];
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[0].length; j++){
+                rowMax[i] = Math.max(rowMax[i], grid[i][j]);
+                colMax[j] = Math.max(colMax[j], grid[i][j]);
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[0].length; j++){
+                sum += Math.min(rowMax[i], colMax[j]) - grid[i][j];
+            }
+        }
+        return sum;
+    }
     public int maxIncreaseKeepingSkyline(int[][] grid) {
         // find max in col and row, get min from that.
         int sum = 0;
