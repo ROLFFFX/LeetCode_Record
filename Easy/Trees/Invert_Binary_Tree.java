0ed8efdada18e68e235c196c0ae527acb648a459
Yuxuan Shi <1051268839@qq.com>
2024-01-17 13:45:33 -0500

    Create Invert_Binary_Tree.java

---
+++ Easy/Trees/Invert_Binary_Tree.java
@@ -0,0 +1,13 @@
+package Easy.Trees;
+
+public class Invert_Binary_Tree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        return root;
+    }
+}
