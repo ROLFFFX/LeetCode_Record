eeb2d6371df18a513e1e4d4563e8b72fe508649f
Yuxuan Shi <1051268839@qq.com>
2024-01-17 08:49:54 -0500

    Create Perfect_Number.java

---
+++ Easy/Perfect_Number.java
@@ -0,0 +1,13 @@
+package Easy;
+
+public class Perfect_Number {
+    public boolean checkPerfectNumber(int num) {
+        if (num % 2 != 0) return false;
+        int sum = 0;
+        for (int i = 1; i < num; i++){
+            if (num % i == 0) sum+=i;
+            if (sum > num) return false;
+        }
+        return sum == num;
+    }
+}
