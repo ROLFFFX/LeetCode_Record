f5fc99e006f3494a66870218f3f081423d2f5859
Yuxuan Shi <1051268839@qq.com>
2024-01-18 16:38:48 -0500

    Created Minimum_Common_Value

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Find Subarrays With Equal Sum";
+        String s = "Minimum Common Value";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
