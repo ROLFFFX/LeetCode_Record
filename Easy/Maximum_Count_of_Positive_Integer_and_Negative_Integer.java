820951990d3e036137fe2f7a73ba5c81aaf1e766
Yuxuan Shi <1051268839@qq.com>
2024-01-17 21:55:39 -0500

    Update Maximum_Count_of_Positive_Integer_and_Negative_Integer.java

--- Easy/Maximum_Count_of_Positive_Integer_and_Negative_Integer.java
+++ Easy/Maximum_Count_of_Positive_Integer_and_Negative_Integer.java
@@ -1,6 +1,19 @@
 package Easy;
 
 public class Maximum_Count_of_Positive_Integer_and_Negative_Integer {
+    public int maximumCount_optimized(int[] nums) {
+        int n = 0;
+        int zero = 0;
+        for (int num : nums){
+            if (num < 0) {
+                n++;
+                continue;
+            }
+            if (num > 0) break;
+            zero++;
+        }
+        return Math.max(n, nums.length - zero - n);
+    }
     public int maximumCount(int[] nums) {
         int p = 0;
         int n = 0;
