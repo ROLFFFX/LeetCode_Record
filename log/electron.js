57d17cd938dfcd319f1af702bc382e00a1e7c8f2
Yuxuan Shi <1051268839@qq.com>
2024-01-18 19:50:04 -0500

    Update electron settings

---
+++ log/electron.js
@@ -0,0 +1,28 @@
+import { app, BrowserWindow } from "electron";
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 400,
+    height: 400,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:5173/");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
