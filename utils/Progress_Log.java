a55b7de83bcdd1725ca83ab82bff1b55b03f61c9
Yuxuan Shi <1051268839@qq.com>
2024-01-19 22:47:53 -0500

    Update Sum_of_Subarray_Minimums

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Minimum Falling Path Sum";
+        String s = "Sum of Subarray Minimums";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
