032a37eeb18cee48f4580d459bcc616fb0da170a
Yuxuan Shi <1051268839@qq.com>
2024-01-20 11:06:34 -0500

    Created Divide_an_Array_Into_Subarrays_With_Minimum_Cost_I

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Number of Strings That Appear as Substrings in Word";
+        String s = "Divide an Array Into Subarrays With Minimum Cost I";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
