6af13320df0fb75aea1d8032435a153d1fd3b92e
Yuxuan Shi <1051268839@qq.com>
2024-01-20 20:01:28 -0500

    Update Maximum_Depth_of_Binary_Tree

--- utils/Draft.java
+++ utils/Draft.java
@@ -1,9 +1,20 @@
 package utils;
 
+import java.util.Stack;
+
 public class Draft {
     public static void main(String[] args) {
-        String s = "a, a, a, a, b,b,b,c, c";
-        String[] arr = s.split("[!?',;. ]");
-        for (String i : arr) System.out.println(i);
+//        String s = "a, a, a, a, b,b,b,c, c";
+//        String[] arr = s.split("[!?',;. ]");
+//        for (String i : arr) System.out.println(i);
+        ifStack();
+    }
+
+    public static void ifStack(){
+        Stack<Integer> stack = new Stack<>();
+        if (stack.add(2)){
+            System.out.println("added");
+        }
+        for (int num : stack) System.out.println(num);
     }
 }
--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Minimize Length of Array Using Operations";
+        String s = "Maximum Depth of Binary Tree";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
