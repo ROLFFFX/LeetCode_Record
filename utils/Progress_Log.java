55d14dc55737c11916c9f648003fb36ba2004b82
Yuxuan Shi <1051268839@qq.com>
2024-01-21 20:28:26 -0500

    Zigzag conversion

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Set Mismatch";
+        String s = "Zigzag Conversion";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
