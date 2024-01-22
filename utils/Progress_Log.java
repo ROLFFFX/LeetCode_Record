9e9cdce1ef81ab12685f63d250eb5c26b3c37c4c
Yuxuan Shi <1051268839@qq.com>
2024-01-22 11:39:19 -0500

    Rearrange_Array_Elements_by_Sign

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Check If a Word Occurs As a Prefix of Any Word in a Sentence";
+        String s = "Rearrange Array Elements by Sign";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
