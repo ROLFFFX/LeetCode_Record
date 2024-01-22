7a7c83c16fe1cf3f0411039170db40db2351a22c
Yuxuan Shi <1051268839@qq.com>
2024-01-21 20:53:30 -0500

    Update RomanToInteger_5_13.java

--- Easy/RomanToInteger_5_13.java
+++ Easy/RomanToInteger_5_13.java
@@ -3,14 +3,43 @@ package Easy;
 public class RomanToInteger_5_13 {
     public static void main(String[] args) {
 ////        I             1
-//        V             5
+////        V             5
 ////        X             10
-//        L             50
+////        L             50
 ////        C             100
-//        D             500
-//        M             1000
+////        D             500
+////        M             1000
         String s = "DCXXI";
         System.out.println(romanToInt(s));
+    }
+
+    public int romanToInt_optimized(String s) {
+        int ans = 0;
+        int prev = getValue(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            int curr = getValue(s.charAt(i));
+            if (curr > prev) {
+                ans -= prev;
+            } else {
+                ans += prev;
+            }
+            prev = curr;
+        }
+        ans += prev;
+        return ans;
+    }
+
+    public int getValue(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 
     /** failed attempt **/
