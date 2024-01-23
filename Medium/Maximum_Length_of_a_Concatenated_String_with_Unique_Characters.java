c51b9eb751f32ff422ea6ead0f17716c5c1e0210
Yuxuan Shi <1051268839@qq.com>
2024-01-22 22:45:07 -0500

    Maximum_Length_of_a_Concatenated_String_with_Unique_Characters

---
+++ Medium/Maximum_Length_of_a_Concatenated_String_with_Unique_Characters.java
@@ -0,0 +1,29 @@
+package Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Maximum_Length_of_a_Concatenated_String_with_Unique_Characters {
+    public int maxLength(List<String> arr) {
+        int max_len = 0;
+        List<Integer> unique = new ArrayList<>();
+        for (String s : arr){
+            Integer bin = 0;
+            for (char c : s.toCharArray()){
+                bin += 1 << (c - 'a');
+                if (Integer.bitCount(bin) == s.length()) unique.add(bin);
+            }
+        }
+        ArrayList<Integer> concat = new ArrayList<>(List.of(0));
+        for (Integer u : unique)
+            for (int i = concat.size() - 1; i >= 0; i--)
+                if ((concat.get(i) & u) == 0)
+                {
+                    Integer cc = concat.get(i) | u;
+                    concat.add(cc);
+                    max_len = Math.max(max_len, Integer.bitCount(cc));
+                }
+
+        return max_len;
+    }
+}
--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Minimum Distance Between BST Nodes";
+        String s = "Maximum Length of a Concatenated String with Unique Characters";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
