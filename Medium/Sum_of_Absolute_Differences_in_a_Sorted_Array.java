bb49edbaa8bf9d7fda9ef7e75924520515d222a2
Yuxuan Shi <1051268839@qq.com>
2024-01-24 13:02:26 -0500

    Update Sum_of_Absolute_Differences_in_a_Sorted_Array.java

--- Medium/Sum_of_Absolute_Differences_in_a_Sorted_Array.java
+++ Medium/Sum_of_Absolute_Differences_in_a_Sorted_Array.java
@@ -1,6 +1,22 @@
 package Medium;
 
 public class Sum_of_Absolute_Differences_in_a_Sorted_Array {
+    public int[] getSumAbsoluteDifferences_optimized(int[] nums) {
+        int left = 0;
+        int right = 0;
+        for (int i = 1; i < nums.length; i++){
+            right += nums[i];
+        }
+        int[] ans = new int[nums.length];
+        ans[0] = right - (nums.length - 1) * nums[0];
+        for (int i = 1; i < nums.length; i++){
+            left += nums[i - 1];
+            right -= nums[i];
+            ans[i] = (i * nums[i] - left) + (right - (nums.length - i - 1) * nums[i]);
+        }
+        return ans;
+    }
+    // tle
     public int[] getSumAbsoluteDifferences(int[] nums) {
         int[] ans = new int[nums.length];
         for (int i = 0; i < nums.length; i++){
