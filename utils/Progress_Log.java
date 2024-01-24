0dc5445fbcc07632e48b63537ef70ed242259e66
Yuxuan Shi <1051268839@qq.com>
2024-01-24 13:37:45 -0500

    The_kth_Factor_of_n

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Optimal Partition of String";
+        String s = "The kth Factor of n";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
