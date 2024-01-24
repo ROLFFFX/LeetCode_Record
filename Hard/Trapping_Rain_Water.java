ada49d358f85e1c2a457cca4b9af8a48e8d76924
Yuxuan Shi <1051268839@qq.com>
2024-01-24 16:48:39 -0500

    Trapping Rain Water

---
+++ Hard/Trapping_Rain_Water.java
@@ -0,0 +1,23 @@
+package Hard;
+
+import java.util.Stack;
+
+public class Trapping_Rain_Water {
+    public int trap(int[] height) {
+        if (height == null || height.length < 2) return 0;
+        int water = 0, i = 0;
+        Stack<Integer> stack = new Stack();
+        while (i < height.length){
+            if (stack.isEmpty() || height[i] <= height[stack.peek()]){
+                stack.push(i++);
+            } else {
+                int pre = stack.pop();
+                if (!stack.isEmpty()){
+                    int minHeight = Math.min(height[stack.peek()], height[i]);
+                    water += (minHeight - height[pre]) * (i - stack.peek() - 1);
+                }
+            }
+        }
+        return water;
+    }
+}
