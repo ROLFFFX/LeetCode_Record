2bfece467b3f6c72a2a4c9498d355fb76968fbff
Yuxuan Shi <1051268839@qq.com>
2024-01-31 10:36:17 -0500

    Count Prefixes of a Given String

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Maximum Sum With Exactly K Elements ";
+        String s = "Count Prefixes of a Given String";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
