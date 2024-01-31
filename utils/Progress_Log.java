b2baff0e22dc38e09dd927633c807e4aef9bfaf9
Yuxuan Shi <1051268839@qq.com>
2024-01-31 01:54:55 -0500

    Minimize_String_Length

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Array Partition";
+        String s = "Minimize String Length";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
