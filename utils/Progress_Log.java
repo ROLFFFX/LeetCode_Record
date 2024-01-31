7cff53354e176a23860b6021bf3114af27d7b288
Yuxuan Shi <1051268839@qq.com>
2024-01-31 01:46:10 -0500

    Array_Partition

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Check If String Is a Prefix of Array";
+        String s = "Array Partition";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
