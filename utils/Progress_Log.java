f775bf0d28d982cbf7847a5842dfef67a4100d10
Yuxuan Shi <1051268839@qq.com>
2024-01-31 13:58:08 -0500

    Largest_Submatrix_With_Rearrangements

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Find the Divisibility Array of a String";
+        String s = "Largest Submatrix With Rearrangements";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
