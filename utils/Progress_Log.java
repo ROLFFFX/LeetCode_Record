e2a2149f60d20346d0a7616321e38074bbb71edb
Yuxuan Shi <1051268839@qq.com>
2024-02-03 19:38:45 -0500

    Sorting_the_Sentence

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Minimum Operations to Make the Array Increasing";
+        String s = "Sorting the Sentence";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
