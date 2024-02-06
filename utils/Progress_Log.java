7ab38fd4dca82fab0f0e6b093af94dea9fd64b79
Yuxuan Shi <1051268839@qq.com>
2024-02-06 14:35:44 -0500

    N_th_Tribonacci_Number

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Path Sum II";
+        String s = "N-th Tribonacci Number";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
