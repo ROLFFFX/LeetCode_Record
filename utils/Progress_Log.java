66711e0bd8cca2b65b9822305ce6108df61d42f8
Yuxuan Shi <1051268839@qq.com>
2024-02-05 20:50:13 -0500

    Delete_Node_in_a_BST

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Longest Palindrome by Concatenating Two Letter Words";
+        String s = "Delete Node in a BST";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
