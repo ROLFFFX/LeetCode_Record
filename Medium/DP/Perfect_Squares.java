ee864403b440054affbd64f0abbf69c20ba522d7
Yuxuan Shi <1051268839@qq.com>
2024-02-08 00:59:15 -0500

    Perfect_Squares

---
+++ Medium/DP/Perfect_Squares.java
@@ -0,0 +1,20 @@
+package Medium.DP;
+
+import java.util.Arrays;
+
+public class Perfect_Squares {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        int count = 1;
+        while (count * count <= n) {
+            int sq = count * count;
+            for (int i = sq; i <= n; i++) {
+                dp[i] = Math.min(dp[i - sq] + 1, dp[i]);
+            }
+            count++;
+        }
+        return dp[n];
+    }
+}
--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Min Cost Climbing Stairs";
+        String s = "Perfect Squares";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
