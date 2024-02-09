790b4aa8ee3d6994dd0351a913c2694e9d2325b0
Yuxuan Shi <1051268839@qq.com>
2024-02-09 15:02:12 -0500

    Largest_Divisible_Subset

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Perfect Squares";
+        String s = "Largest Divisible Subset";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
