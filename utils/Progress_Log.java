847457949e71906bf1e1be0ce9042547469765ae
Yuxuan Shi <1051268839@qq.com>
2024-02-13 20:12:00 -0500

    Rearrange_Array_Elements_by_Sign

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Largest Divisible Subset";
+        String s = "Rearrange Array Elements by Sign";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
