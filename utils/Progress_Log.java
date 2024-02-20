141758f9a645f1172ece2f5095515e96f0184840
Yuxuan Shi <1051268839@qq.com>
2024-02-19 23:03:58 -0500

    Missing_Number

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Power of Two";
+        String s = "Missing Number";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
