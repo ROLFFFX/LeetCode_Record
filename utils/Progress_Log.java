50ad013f00889eac019c66330dd194ac5c0d5b31
Yuxuan Shi <1051268839@qq.com>
2024-02-21 21:37:27 -0500

    Find_the_Town_Judge

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Bitwise AND of Numbers Range";
+        String s = "Find the Town Judge";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
