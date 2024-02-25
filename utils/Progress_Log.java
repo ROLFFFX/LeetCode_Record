b4c5ebc8dc99d19e3724e37c3bf0ad0e80ae9ca3
Yuxuan Shi <1051268839@qq.com>
2024-02-24 20:38:27 -0500

    Greatest_Common_Divisor_Traversal

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Find All People With Secret";
+        String s = "Greatest Common Divisor Traversal";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
