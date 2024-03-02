2cb0c064388e0a3dc1dd38cd5d8874d0d6605fe1
Yuxuan Shi <1051268839@qq.com>
2024-03-02 00:01:37 -0500

    Update Squares_of_a_Sorted_Array.java

--- Easy/Squares_of_a_Sorted_Array.java
+++ Easy/Squares_of_a_Sorted_Array.java
@@ -1,4 +1,13 @@
 package Easy;
 
+import java.util.Arrays;
+
 public class Squares_of_a_Sorted_Array {
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++){
+            nums[i] *= nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
 }
