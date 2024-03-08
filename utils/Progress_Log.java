ae13333e5e07d28dc77c816f813e3fbf6276c298
Yuxuan Shi <1051268839@qq.com>
2024-03-07 20:08:08 -0500

    Count_Elements_With_Maximum_Frequency

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Linked List Cycle";
+        String s = "Count Elements With Maximum Frequency";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
