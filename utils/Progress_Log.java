489572d800b77f6fd5e693550486257bc87d2fdb
Yuxuan Shi <1051268839@qq.com>
2024-03-09 03:30:58 -0600

    Minimum_Common_Value

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Binary Search";
+        String s = "Minimum Common Value";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
