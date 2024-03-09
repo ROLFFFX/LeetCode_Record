581a1aeb30fc0f6509cd6405d78364d2c7b34d1b
Yuxuan Shi <1051268839@qq.com>
2024-03-09 03:28:06 -0600

    Binary_Search

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Count Complete Tree Nodes";
+        String s = "Binary Search";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
