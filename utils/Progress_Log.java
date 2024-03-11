6867655e01dc8a59ed23e9fa57942fa373ac6851
Yuxuan Shi <1051268839@qq.com>
2024-03-10 20:25:26 -0600

    Custom_Sort_String

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Find Smallest Letter Greater Than Target";
+        String s = "Custom Sort String";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
