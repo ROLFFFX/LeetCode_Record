3c1feafe1f67aeccd11eab869e0738ba45df42ad
Yuxuan Shi <1051268839@qq.com>
2024-03-12 18:54:54 -0600

    Find_the_pivot_integer

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Repeated DNA Sequences";
+        String s = "Find the pivot integer";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
