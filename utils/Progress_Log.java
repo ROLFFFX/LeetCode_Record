6543c78a09a07950fe965f2b78fe3ebadea6acc4
Yuxuan Shi <1051268839@qq.com>
2024-03-15 21:43:15 -0600

    Contiguous Array

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Product of Array Except Self";
+        String s = "Contiguous Array";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
