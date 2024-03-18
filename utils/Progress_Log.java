eb0fbd70ace77869d31d91e0389efdb13be3b619
Yuxuan Shi <1051268839@qq.com>
2024-03-17 18:55:12 -0600

    Minimum Number of Arrows to Burst Balloons

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Intersection of Three Sorted Arrays";
+        String s = "Minimum Number of Arrows to Burst Balloons";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
