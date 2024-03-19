adf1668793fa55ede35cebc8bc91a844782fb9a3
Yuxuan Shi <1051268839@qq.com>
2024-03-19 01:22:59 -0400

    Sort_Array_by_Increasing_Frequency

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Task Scheduler";
+        String s = "Sort Array by Increasing Frequency";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
