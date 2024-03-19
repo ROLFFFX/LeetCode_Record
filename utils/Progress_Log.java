66c6615d3430914fe1351888c932270ef6bf5c41
Yuxuan Shi <1051268839@qq.com>
2024-03-18 22:35:47 -0400

    Task Scheduler

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Minimum Number of Arrows to Burst Balloons";
+        String s = "Task Scheduler";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
