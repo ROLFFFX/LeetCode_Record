e6408ba70e6d6fc598e20d7bbdf8423c3ca8ac00
Yuxuan Shi <1051268839@qq.com>
2024-03-21 02:25:58 -0400

    Jump Game

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Best Time to Buy and Sell Stock II";
+        String s = "Jump Game";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
