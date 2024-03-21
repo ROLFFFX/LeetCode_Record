7b428c4abf15ace58aea9c27696c17c5629d1478
Yuxuan Shi <1051268839@qq.com>
2024-03-21 10:12:34 -0400

    Insert_Delete_GetRandom_O(1)

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "H-Index";
+        String s = "Insert Delete GetRandom O(1)";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
