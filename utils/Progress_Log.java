3f5fdc3a5a94fac9f2cdd6724e287c5a4b6392f4
Yuxuan Shi <1051268839@qq.com>
2024-03-23 20:14:07 -0400

    Find the Duplicate Number

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Palindrome Linked List";
+        String s = "Find the Duplicate Number";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
