cfb0e6184e5e920be9c95106be396d867be372ac
Yuxuan Shi <1051268839@qq.com>
2024-03-25 11:44:13 -0400

    Ransom Note

---
+++ Easy/Ransom_Note.java
@@ -0,0 +1,15 @@
+package Easy;
+
+public class Ransom_Note {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] bucket = new int[26];
+        for (char c : magazine.toCharArray()){
+            bucket[c - 'a']++;
+        }
+        for (char c : ransomNote.toCharArray()){
+            bucket[c - 'a']--;
+            if (bucket[c - 'a'] < 0) return false;
+        }
+        return true;
+    }
+}
