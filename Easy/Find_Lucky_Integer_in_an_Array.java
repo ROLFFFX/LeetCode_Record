81cd419f94ff708ebe8d6d9e3cbabe9e6070d4d3
Yuxuan Shi <1051268839@qq.com>
2024-03-28 13:52:19 -0400

    Update Find_Lucky_Integer_in_an_Array.java

--- Easy/Find_Lucky_Integer_in_an_Array.java
+++ Easy/Find_Lucky_Integer_in_an_Array.java
@@ -4,6 +4,15 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class Find_Lucky_Integer_in_an_Array {
+    public int findLucky_opt(int[] arr) {
+        int[] b = new int[501];
+        for (int n : arr) b[n]++;
+        int cand = -1;
+        for (int n : arr) {
+            if (b[n] == n) cand = Math.max(cand, n);
+        }
+        return cand;
+    }
     public int findLucky(int[] arr) {
         HashMap<Integer, Integer> map = new HashMap();
         for (int n : arr) {
