1550d381c4161d7882abbde9716ee77855ad28be
Yuxuan Shi <1051268839@qq.com>
2024-04-03 21:23:01 -0400

    Maximum_Nesting_Depth_of_the_Parentheses

---
+++ Easy/Maximum_Nesting_Depth_of_the_Parentheses.java
@@ -0,0 +1,14 @@
+package Easy;
+
+public class Maximum_Nesting_Depth_of_the_Parentheses {
+    public int maxDepth(String s) {
+        int count = 0;
+        int max = 0;
+        for (char c : s.toCharArray()){
+            if (c == '(') count++;
+            if (c == ')') count--;
+            max = Math.max(count, max);
+        }
+        return max;
+    }
+}
--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Subarrays with K Different Integers";
+        String s = "Maximum Nesting Depth of the Parentheses";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
