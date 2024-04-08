b6f216a65b67f87968a761487c60a9e77f409bdf
Yuxuan Shi <1051268839@qq.com>
2024-04-07 23:53:29 -0400

    Number_of_Students_Unable_to_Eat_Lunch

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Divisible and Non-divisible Sums Difference";
+        String s = "Number of Students Unable to Eat Lunch";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
