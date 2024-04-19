8d861220c20246d80c5bce8fdce3e25405290e0e
Yuxuan Shi <1051268839@qq.com>
2024-04-19 03:55:29 -0400

    Update Maximum_Number_of_Integers_to_Choose_From_a_Range_I.java

--- Medium/Maximum_Number_of_Integers_to_Choose_From_a_Range_I.java
+++ Medium/Maximum_Number_of_Integers_to_Choose_From_a_Range_I.java
@@ -3,6 +3,19 @@ package Medium;
 import java.util.HashSet;
 
 public class Maximum_Number_of_Integers_to_Choose_From_a_Range_I {
+    public int maxCount_opt(int[] banned, int n, int maxSum) {
+        HashSet<Integer> set = new HashSet();
+        for (int ele : banned) set.add(ele);
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (set.contains(i)) continue;
+            if (i <= maxSum) {
+                count ++;
+                maxSum -= i;
+            } else break;
+        }
+        return count;
+    }
     public int maxCount(int[] banned, int n, int maxSum) {
         HashSet<Integer> set = new HashSet();
         HashSet<Integer> ans = new HashSet();
