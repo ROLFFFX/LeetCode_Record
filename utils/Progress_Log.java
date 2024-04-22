197004b8dbc5ba6fddabba68cd0e1e4a1a8fc94f
Yuxuan Shi <1051268839@qq.com>
2024-04-21 23:12:14 -0400

    Find_if_Path_Exists_in_Graph

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Find All Groups of Farmland";
+        String s = "Find if Path Exists in Graph";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
