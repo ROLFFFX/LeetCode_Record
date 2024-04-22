01242726c4eccb6937474a3d6fb18e6dcb56275a
Yuxuan Shi <1051268839@qq.com>
2024-04-22 01:08:06 -0400

    Update Find_if_Path_Exists_in_Graph.java

--- Easy/Find_if_Path_Exists_in_Graph.java
+++ Easy/Find_if_Path_Exists_in_Graph.java
@@ -3,6 +3,31 @@ package Easy;
 import java.util.*;
 
 public class Find_if_Path_Exists_in_Graph {
+    public boolean validPath_opt(int n, int[][] edges, int source, int destination) {
+        if (source == destination) return true;
+        HashSet<Integer> visited = new HashSet();
+        Queue<Integer> q = new LinkedList();
+        q.add(source);
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            for (int i = 0; i < sz; i++) {
+                int curr = q.poll();
+                if (!visited.add(curr)) continue;
+                for (int[] edge : edges) {
+                    if (edge[0] == curr) {
+                        if (edge[1] == destination) return true;
+                        q.add(edge[1]);
+                    }
+                    if (edge[1] == curr) {
+                        if (edge[0] == destination) return true;
+                        q.add(edge[0]);
+                    }
+
+                }
+            }
+        }
+        return false;
+    }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
         for (int[] edge : edges) {
