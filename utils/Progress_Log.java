2c7b6f131517e2794ed190208caf77cc2bc395ea
Yuxuan Shi <1051268839@qq.com>
2024-04-23 10:01:40 -0400

    Minimum_Height_Trees

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Count the Number of Special Characters II";
+        String s = "Minimum Height Trees";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
