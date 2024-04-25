e63fcf377767139b9cbd6527af09ee40041ebc71
Yuxuan Shi <1051268839@qq.com>
2024-04-24 22:08:02 -0400

    Generate Parenthesis

---
+++ Medium/Generate_Parentheses.java
@@ -0,0 +1,29 @@
+package Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generate_Parentheses {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        backtracking(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    public void backtracking(List<String> ans, StringBuilder sb, int left, int right, int n) {
+        if (sb.length() == 2 * n) {
+            ans.add(sb.toString());
+            return;
+        }
+        if (left < n) {
+            sb.append("(");
+            backtracking(ans, sb, left + 1, right, n);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        if (left > right) {
+            sb.append(")");
+            backtracking(ans, sb, left, right + 1, n);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Open the Lock";
+        String s = "Generate Parentheses";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
