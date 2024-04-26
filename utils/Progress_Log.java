605ac951377dc68dae316b53edbf35108683fdeb
Yuxuan Shi <1051268839@qq.com>
2024-04-25 23:18:03 -0400

    Minimum_Falling_Path_Sum_II

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Generate Parentheses";
+        String s = "Minimum Falling Path Sum II";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
