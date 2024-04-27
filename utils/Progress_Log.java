ea252badb91dc374b719454d931103d83a53d882
Yuxuan Shi <1051268839@qq.com>
2024-04-26 20:31:57 -0400

    Freedom_Trail

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Minimum Falling Path Sum II";
+        String s = "Freedom Trail";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
