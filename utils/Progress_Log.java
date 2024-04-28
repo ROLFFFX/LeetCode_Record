81f730b8d20bee2da70651ce068c959986e0cb65
Yuxuan Shi <1051268839@qq.com>
2024-04-27 21:02:04 -0400

    Sum of Distances in Tree

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Freedom Trail";
+        String s = "Sum of Distances in Tree";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
