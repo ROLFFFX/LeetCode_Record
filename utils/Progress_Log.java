28b9d24f5b75c2dc1697fea49fd891c3bd708257
Yuxuan Shi <1051268839@qq.com>
2024-04-29 01:07:45 -0400

    Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Sum of Distances in Tree";
+        String s = "Minimum Number of Operations to Make Array XOR Equal to K";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
