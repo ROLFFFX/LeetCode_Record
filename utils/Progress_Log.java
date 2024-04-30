f33121e9f97060f68047e6e0db054294c3f64c58
Yuxuan Shi <1051268839@qq.com>
2024-04-29 23:29:57 -0400

    Linked_List_Frequency

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Jewels and Stones";
+        String s = "Linked List Frequency";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
