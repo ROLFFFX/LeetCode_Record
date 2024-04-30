fb4cedc6d0ebef2346a3bef777c7b7b78dbe03ad
Yuxuan Shi <1051268839@qq.com>
2024-04-29 22:57:17 -0400

    Jewels_and_Stones

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Minimum Number of Operations to Make Array XOR Equal to K";
+        String s = "Jewels and Stones";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
