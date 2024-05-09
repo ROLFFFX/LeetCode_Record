a9ce9d22a1b7aba7ab140c36528effeab1beae22
Yuxuan Shi <1051268839@qq.com>
2024-05-08 23:02:24 -0400

    Maximize_Happiness_of_Selected_Children

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Gold Medal";
+        String s = "Maximize Happiness of Selected Children";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
