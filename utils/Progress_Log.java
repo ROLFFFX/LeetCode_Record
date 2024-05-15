2a05aca8167a34e2f3880b20644e15068beecfb2
Yuxuan Shi <1051268839@qq.com>
2024-05-14 21:03:45 -0700

    safest path

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Path with Maximum Gold";
+        String s = "Safest Path";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
