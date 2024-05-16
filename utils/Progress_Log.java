d73ffdc0cf941e574d84f46af4fe7a6d0d834dc2
Yuxuan Shi <1051268839@qq.com>
2024-05-16 16:58:25 +0900

    evaluate_tree

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Safest Path";
+        String s = "evaluate tree";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
