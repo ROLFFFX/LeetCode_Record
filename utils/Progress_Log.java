4a80502f2e657e7cc1f46097928d466fc15b55eb
Yuxuan Shi <1051268839@qq.com>
2024-05-17 11:27:26 +0900

    remove leaf node

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "evaluate tree";
+        String s = "remove leaf node";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
