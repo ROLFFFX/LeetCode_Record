5f9acfcc601032a7612d0a5eb109d3be857e3387
Yuxuan Shi <1051268839@qq.com>
2024-05-19 15:27:29 +0800

    Find_the_Maximum_Sum_of_Node_Values

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "remove leaf node";
+        String s = "Find the Maximum Sum of Node Values\n";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
