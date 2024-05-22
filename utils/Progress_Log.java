00ba7896dd91aa8a683762836d1ebc9c7c7b3116
Yuxuan Shi <1051268839@qq.com>
2024-05-22 13:23:42 +0800

    Palindrome_Partitioning

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Find the Maximum Sum of Node Values\n";
+        String s = "Palindrome Partitioning";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
