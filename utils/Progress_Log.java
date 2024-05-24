fb99f58a53efe24ad8eaf362c3d144bc21db03f6
Yuxuan Shi <1051268839@qq.com>
2024-05-24 18:32:03 +0800

    Maximum_Score_Words_Formed_by_Letters

--- utils/Progress_Log.java
+++ utils/Progress_Log.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Progress_Log {
 
     public static void main(String[] args) {
-        String s = "Palindrome Partitioning";
+        String s = "Maximum Score Words Formed by Letters";
         String ans = "";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
